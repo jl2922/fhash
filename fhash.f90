a83be3e7812c7f4f1197b225d2945e3b1e544df4
Aldo Hennink <a.hennink@protonmail.ch>
2021-09-30 12:11:57 +0200

    added finalizers to note_type - Makes 'clear' redundant; memory is deleted automatically

--- fhash.f90
+++ fhash.f90
@@ -115,13 +115,16 @@ module FHASH_MODULE_NAME
       ! Otherwise, fail and return 0
       procedure, non_overridable :: node_remove
       
+      ! Return the length of the linked list start from the current node.
+      procedure, non_overridable :: node_depth
+      
       ! Deallocate kv if allocated.
       ! Call the clear method of the next node if the next pointer associated.
       ! Deallocate and nullify the next pointer.
-      procedure, non_overridable :: node_clear
-
-      ! Return the length of the linked list start from the current node.
-      procedure, non_overridable :: node_depth
+      !
+      ! Need separate finalizers because a resursive procedure cannot be elemental.
+      final :: clear_scalar_node
+      final :: clear_rank1_nodes
   end type
 
   type FHASH_TYPE_NAME
@@ -153,7 +156,7 @@ module FHASH_MODULE_NAME
       ! Remove the value with the given key.
       procedure, non_overridable, public :: remove
 
-      ! Clear all the allocated memory (must be called to prevent memory leak).
+      ! Clear all the allocated memory
       procedure, non_overridable, public :: clear
   end type
 
@@ -347,28 +350,25 @@ module FHASH_MODULE_NAME
   end subroutine
 
   subroutine clear(this)
-    class(FHASH_TYPE_NAME), intent(inout) :: this
-    integer :: i
-
-    if (.not. allocated(this%buckets)) return
-
-    do i = 1, size(this%buckets)
-      if (associated(this%buckets(i)%next)) then
-        call this%buckets(i)%next%node_clear()
-        deallocate(this%buckets(i)%next)
-      endif
-    enddo
-    deallocate(this%buckets)
-    this%n_keys = 0
-    this%n_buckets = 0
+    class(FHASH_TYPE_NAME), intent(out) :: this
   end subroutine
 
-  recursive subroutine node_clear(this)
-    class(node_type), intent(inout) :: this
+  subroutine clear_rank1_nodes(nodes)
+    type(node_type), intent(inout) :: nodes(:)
 
-    if (associated(this%next)) then
-      call this%next%node_clear()
-      deallocate(this%next)
+    integer :: i
+
+    do i = 1, size(nodes)
+      call clear_scalar_node(nodes(i))
+    enddo
+  end subroutine
+
+  recursive subroutine clear_scalar_node(node)
+    type(node_type), intent(inout) :: node
+
+    if (associated(node%next)) then
+      call clear_scalar_node(node%next)
+      deallocate(node%next)
     endif
   end subroutine
 
