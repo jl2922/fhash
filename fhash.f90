dca46f3afd9e42a01009301bdaa88ceacd301be9
Aldo Hennink <aldo.hennink@vortech.nl>
2021-10-12 16:16:25 +0200

    addd assertion: at least one bucket

--- fhash.f90
+++ fhash.f90
@@ -249,6 +249,7 @@ module FHASH_MODULE_NAME
     & 573292817, 1164186217, 2147483647]
 
     call assert(this%key_count() == 0, 'Cannot reserve when fhash is not empty.')
+    call assert(n_buckets >= 1, "I need at least one bucket.")
     call assert(sizes(size(sizes)) >= n_buckets, "Did not expect to need this many buckets.")
 
     do i = 1, size(sizes)
